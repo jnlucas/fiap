8ddcf62c48e314539a2d37dc7dccd2b88e681485
Gabriel Nascimento de Oliveira <gndeoliveira@outlook.com>
2022-05-09 11:59:02 -0300

    Resolvendo bug do modal

--- script.js
+++ script.js
@@ -1,73 +1,53 @@
-
-function pegaDados(){
-    console.log('pega dados funcionando');
-    
-    
-    $.ajax({
-        url: "https://iot.14mob.com/lista.json",
-        data: "",
-        success: function(retorno){
-            tratarDados(retorno);
-        },
-        dataType: "html"
-    })
-
-
+function pegaDados() {
+	$.ajax({
+		url: "https://iot.14mob.com/lista.json",
+		data: "",
+		success: function (retorno) {
+			tratarDados(retorno);
+		},
+		dataType: "html"
+	});
 }
 
-function tratarDados(dados){
+function tratarDados(dados) {
+	dados = JSON.parse(dados);
 
-    dados = JSON.parse(dados);
+	var lista = $('.listaItens');
 
-    var lista = $('.listaItens')
-    
-    $(dados).each(function(index,valor){
-        
-       let conteudo = `<div class="col-md-4">
+	$(dados).each(function (index, valor) {
+		let conteudo = `<div class="col-md-4">
                 <div class="card" >
-                    <img src="${valor.imagem}" class="card-img-top" onclick="abrirModal()" alt="tertertert">
+                    <img src="${valor.imagem}" class="card-img-top" onclick="abrirModal('${valor.imagem}','${valor.nome}','${valor.descricao}')" alt="tertertert">
                     <div class="card-body">
-                    <h5 class="card-title">${valor.nome}</h5>
-                    <p class="card-text">${valor.descricao}</p>
-                    <button type="button" class="btn btn-primary abrirModal" onclick="abrirModal('${valor.nome}')" >
-                        Detalhes
-                    </button>
-
+                        <h5 class="card-title">${valor.nome}</h5>a
+                        <p class="card-text">${valor.descricao}</p>
+                        <button type="button" class="btn btn-primary abrirModal" onclick="abrirModal('${valor.imagem}','${valor.nome}','${valor.descricao}')" >
+                            Detalhes
+                        </button>
                     </div>
                 </div>
             </div>`;
-
-        lista.append(conteudo);
-    
-
-    })
-
-    
-
+		lista.append(conteudo);
+	})
 }
 
-
-function abrirModal(id){
-
-    let conteudo = `<div class="col-md-12">
+function abrirModal(imagem, nome, descricao) {
+	let conteudo = `<div class="col-md-12">
                 <div class="card" >
-                    <img src="" class="card-img-top"  alt="tertertert">
+                    <img src="${imagem}" class="card-img-top"  alt="tertertert">
                     <div class="card-body">
-                    <h5 class="card-title">nome</h5>
-                    <p class="card-text">descricao</p>
+                    <h5 class="card-title">${nome}</h5>
+                    <p class="card-text">${descricao}</p>
                 </div>
                 </div>
             </div>`;
 
-    $('.detalheItem').html(conteudo)   
+	$('.detalheItem').html(conteudo);
 
-    $('#exampleModal').modal('show');
+	$('#exampleModal').modal('show');
 
 }
 
-
-$(function(){
-    pegaDados();
+$(function () {
+	pegaDados();
 })
-
-
