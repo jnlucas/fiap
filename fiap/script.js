e66f278feb3e74e510f153a895a96319c0b4c837
Nathália Elen <nathaliaelen79@gmail.com>
2022-05-09 11:39:45 -0300

    Atualizando o código do João

---
+++ fiap/script.js
@@ -0,0 +1,75 @@
+
+function pegaDados(){
+    console.log('pega dados funcionando');
+    
+    
+    $.ajax({
+        url: "https://iot.14mob.com/lista.json",
+        data: "",
+        success: function(retorno){
+            tratarDados(retorno);
+        },
+        dataType: "html"
+    })
+
+
+}
+
+function tratarDados(dados){
+
+    dados = JSON.parse(dados);
+
+    var lista = $('.listaItens')
+    
+    $(dados).each(function(index,valor){
+        
+       let conteudo = `<div class="col-md-4">
+                <div class="card" >
+                    <img src="${valor.imagem}" class="card-img-top" onclick="abrirModal('${valor.nome}','${valor.imagem}','${valor.descricao}')" alt="tertertert">
+                    <div class="card-body">
+                    <h5 class="card-title">${valor.nome}</h5>
+                    <p class="card-text">${valor.descricao}</p>
+                    <button type="button" class="btn btn-primary" onclick="abrirModal('${valor.nome}','${valor.imagem}','${valor.descricao}')" >
+                        Detalhes
+                    </button>
+
+                    </div>
+                </div>
+            </div>`;
+
+        lista.append(conteudo);
+    
+
+    })
+
+    
+
+}
+
+
+function abrirModal(nome,imagem,descricao){
+
+    console.log(nome,imagem,descricao);
+
+    let conteudo = `<div class="col-md-12">
+                <div class="card" >
+                    <img src="${imagem}" class="card-img-top"  alt="tertertert">
+                    <div class="card-body">
+                    <h5 class="card-title">${nome}</h5>
+                    <p class="card-text">${descricao}</p>
+                </div>
+                </div>
+            </div>`;
+
+    $('.detalheItem').html(conteudo)   
+
+    $('#exampleModal').modal('show');
+
+}
+
+
+$(function(){
+    pegaDados();
+})
+
+
