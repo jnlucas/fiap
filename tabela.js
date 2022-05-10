8d1ae08335709473b446d09e70d1a8922fa52f9c
joao.lucas <joao.lucas@14mob.com>
2022-05-10 11:53:26 -0300

    criando laço com atributo select

--- tabela.js
+++ tabela.js
@@ -1,3 +1,5 @@
+var retornoGlobal;
+
 function carregar(){
       
     $.ajax({
@@ -5,28 +7,42 @@ function carregar(){
         data: "",
         success: function(retorno){
             tratarDados(retorno);
+            popularSelect(retorno);
+
+            retornoGlobal = retorno;
+
         } ,
         dataType: "json"
     });
 }
 
+function popularSelect(param){
+
+    let select = $('.form-select')
+
+    $(param).each(function(chave,valor){
+        
+        let conteudo = `<option value="${valor.id}">${valor.nome}</option>`;
+
+        select.append(conteudo)
+    })
+    
+
+
+}
 
 function tratarDados(retorno){
 
 
 
     $(retorno).each(function(chave,valor){ //laço que se repete 3 vezez
-        let conteudo = `<tr>
+        let conteudo = `<tr onclick="abrirModal('${valor.nome}','${valor.descricao}','${valor.imagem}')" >
         <td>${valor.id}</td>
         <td><img class="foto" src="${valor.imagem}" />  </td>
         <td>${valor.nome}</td>
         <td>${valor.descricao}</td>
         
-        <td>
-            <button type="button" class="btn btn-primary" onclick="abrirModal()">
-                abrir modal
-            </button>
-        </td>
+        
       </tr>`
 
       let tabela = $('.minhaTabela');
@@ -45,18 +61,50 @@ function addCor(){
 
 }
 
-function abrirModal(){
+
+function selecionaValor(valor){
+
+    //var valor = $('.form-select').val();
+    // for(i = 0; i < retornoGlobal.length; i ++){
+    //     if(valor == retornoGlobal[i].id){
+            
+    //         abrirModal(retornoGlobal[i].nome,retornoGlobal[i].descricao,retornoGlobal[i].imagem)
+
+    //     }  
+    // }
+
+    $(retornoGlobal).each(function(chave,valorLaco){
+        if(valor == valorLaco.id){
+            abrirModal(valorLaco.nome,valorLaco.descricao,valorLaco.imagem)
+        } 
+    })
+    
+}
+
+
+function abrirModal(nome,descricao,imagem){
 
 
      $('#exampleModal').modal("show");
 
     var titulo = $("#exampleModalLabel");
+    titulo.html(nome);
 
-    var body = $(".modal-body");
+    
+    var teste = $(".modal-body");
 
-    //body.html("fdghfdgfdgdfgdgdfg");
-    titulo.html("meu modal joao lucas");
 
+    let conteudo = `<div class="col-md-12">
+        <div class="card" >
+            <img src="${imagem}" class="card-img-top"  alt="tertertert">
+            <div class="card-body">
+            <h5 class="card-title">${nome}</h5>
+            <p class="card-text">${descricao}</p>
+        </div>
+        </div>
+    </div>`;
+    
+    teste.html(conteudo);
 }
 
 carregar();
